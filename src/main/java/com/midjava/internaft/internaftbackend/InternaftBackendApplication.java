bb93b435dcea2f97ced4d9a60fa55c428912f4a6
Швец Илья Юрьевич <mixternnik@gmail.com>
2020-09-18 18:37:49 +0600

    Starting project from template

---
+++ src/main/java/com/midjava/internaft/internaftbackend/InternaftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.midjava.internaft.internaftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternaftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternaftBackendApplication.class, args);
+    }
+
+}
